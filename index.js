8abe9b25fc919b92a8a8866a6028900436a9951d
OleksandrMyliutin <123753201+OleksandrMyliutin@users.noreply.github.com>
2025-02-15 19:38:29 +0000

    Deploying to gh-pages from @ OleksandrMyliutin/goit-js-hw-11@ce4da1bd0e2891bb144245a89caa493200367115 🚀

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import{a as d,S as p,i as n}from"./assets/vendor-D0cagnvz.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&a(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const f=d.create({baseURL:"https://pixabay.com/api/",params:{key:"48847177-927ba4e40b84ac1d7e4adedbb",image_type:"photo",orientation:"horizontal",safesearch:!0}});function m(r){return f.get("",{params:{q:r}}).then(i=>i.data.hits)}function h(r){const{largeImageURL:i,webformatURL:o,tags:a,likes:e,views:t,comments:s,downloads:u}=r;return`<li>
+import{a as u,S as d,i as l}from"./assets/vendor-D0cagnvz.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const f=u.create({baseURL:"https://pixabay.com/api/",params:{key:"48847177-927ba4e40b84ac1d7e4adedbb",image_type:"photo",orientation:"horizontal",safesearch:!0}});function p(r){return f.get("",{params:{q:r}}).then(i=>i.data.hits)}function m(r){const{largeImageURL:i,webformatURL:n,tags:o,likes:e,views:t,comments:a,downloads:c}=r;return`<li>
             <div class="card">
               <a class="gallery-link" href="${i}">
                   <img
                     class="card-image"
-                    src="${o}"
-                    alt="${a}"
+                    src="${n}"
+                    alt="${o}"
                   />
               </a>
               <div class="card-content">
@@ -18,13 +18,13 @@ import{a as d,S as p,i as n}from"./assets/vendor-D0cagnvz.js";(function(){const 
                 </ul>
                 <ul>
                   <li><h5>Comments</h5></li>
-                  <li><p>${s}</p></li>
+                  <li><p>${a}</p></li>
                 </ul>
                 <ul>
                   <li><h5>Downloads</h5></li>
-                  <li><p>${u}</p></li>
+                  <li><p>${c}</p></li>
                 </ul>
               </div>
             </div>
-          </li>`}function g(r){return r.map(h).join("")}const l={cardList:document.querySelector(".card-list"),form:document.querySelector(".form-style"),loader:document.querySelector(".loader")};l.form.addEventListener("submit",b);let y=new p(".card-list a",{captionsData:"alt",captionDelay:250}),c;function b(r){r.preventDefault();const i=r.currentTarget.elements.query.value.trim();if(!i){n.warning({title:"Warning",message:"⚠️ Please enter a search query!",position:"topRight"});return}l.loader.style.display="block",c&&clearInterval(c),c=setTimeout(()=>{m(i).then(o=>{if(!o.length){n.info({title:"Not found",message:"😢 Sorry, there are no images matching your search query. Please try again!",position:"topRight"});return}l.cardList.innerHTML=g(o),y.refresh()}).catch(o=>{n.error({title:"Error",message:"🚨 Sorry, something went wrong. Please try again!",position:"topRight"})}).finally(()=>{l.loader.style.display="none"})},3e3),r.currentTarget.reset()}
+          </li>`}function h(r){return r.map(m).join("")}const s={cardList:document.querySelector(".card-list"),form:document.querySelector(".form-style"),loader:document.querySelector(".loader")};s.form.addEventListener("submit",y);let g=new d(".card-list a",{captionsData:"alt",captionDelay:250});function y(r){r.preventDefault();const i=r.currentTarget.elements.query.value.trim();if(!i){l.warning({title:"Warning",message:"⚠️ Please enter a search query!",position:"topRight"});return}s.cardList.innerHTML="",s.loader.style.display="block";function n(){p(i).then(o=>{if(!o.length){l.info({title:"Not found",message:"😢 No images found.",position:"topRight"}),s.cardList.innerHTML="";return}s.cardList.innerHTML=h(o),g.refresh()}).catch(o=>{l.error({title:"Error",message:"🚨 Something went wrong. Please try again!",position:"topRight"}),console.error("❌ API request error:",o),s.cardList.innerHTML=""}).finally(()=>{s.loader.style.display="none"})}n(),r.currentTarget.reset()}
 //# sourceMappingURL=index.js.map
